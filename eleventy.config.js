d275872b9840bdbc87d163eeb0cf8ca1a640ca45
wendyking <wendy@lumenlearning.com>
2026-02-23 11:43:09 -0500

    Remove CMS admin files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
-  eleventyConfig.addPassthroughCopy("src/admin");
-  eleventyConfig.addPassthroughCopy("src/images");
 
   eleventyConfig.addFilter("dateFormat", function (date, format) {
     const d = new Date(date);
